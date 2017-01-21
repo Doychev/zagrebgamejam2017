35fe761c4f3d536b17a0e2da2c87768f18b6693a
Martin Doychev <martin.doychev@abv.bg>
2017-01-21 17:18:10 +0100

    simple score managing and difficulty increasing

---
+++ ShoutingIsland/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    public static ScoreManager Instance
+    {
+        get;
+        private set;
+    }
+
+    public Text scoreText, peopleText;
+
+    private int currentScore = 0;
+
+	void Awake () {
+        ScoreManager.Instance = this;
+    }
+	
+    public void UpdateScore()
+    {
+        int livingPeople = CrowdManager.Instance.CountLivingHumans();
+        currentScore += livingPeople;
+        scoreText.text = "Score: " + currentScore;
+        peopleText.text = "Alive: " + livingPeople;
+
+        TsunamiManager.Instance.IncreaseDifficulty();
+    }
+}
