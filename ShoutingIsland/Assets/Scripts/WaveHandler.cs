cc280e13337cd456268453d13787bb3717e1aa05
Martin Doychev <martin.doychev@abv.bg>
2017-01-21 03:07:04 +0100

    waves

---
+++ ShoutingIsland/Assets/Scripts/WaveHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        for (int i = 0; i < coll.contacts.Length; i++)
+        {
+            Vector2 diff = Quaternion.AngleAxis(90, Vector3.up) * coll.contacts[i].normal * 2;
+            //coll.contacts[i].point + coll.contacts[i]
+            Debug.Log(coll.contacts[i].normal);
+            Debug.Log(coll.contacts[i].point - diff);
+            Debug.Log(coll.contacts[i].point);
+            Debug.Log(coll.contacts[i].point + diff);
+
+            Debug.DrawLine(coll.contacts[i].point - diff, coll.contacts[i].point + diff, Color.green);
+        }
+        Destroy(gameObject);
+    }
+}
